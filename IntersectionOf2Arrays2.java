3ef3bfd41d330060f3c8765c46a024a7aa1a1ec1
MOHNISH PAWAR <105532254+mohnish10@users.noreply.github.com>
2023-12-30 04:56:50 -0500

    Practice program for 20231230

---
+++ IntersectionOf2Arrays2.java
@@ -0,0 +1,53 @@
+package practiceprogramsforjavadsa;
+
+import java.util.*;
+
+public class IntersectionOf2Arrays2
+{
+
+    public static int intersectionOf2Arrays(int[] a,int[] b,int m,int n)
+    {
+
+        m=a.length;
+
+        n=b.length;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        for(int i=0;i<m;i++)
+        {
+
+            list.add(a[i]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+
+            list1.add(b[i]);
+        }
+
+        Set<Integer> set = new HashSet<>(list);
+
+        Set<Integer> set2 = new HashSet<>(list1);
+
+        set.retainAll(set2);
+
+        Integer[] arrNew = set.toArray(new Integer[set.size()]);
+
+        return arrNew.length;
+
+    }
+
+
+    public static void main(String[] args)
+    {
+
+        int[] a = {89, 24, 75, 11, 23};
+
+        int[] b = {89, 2, 4};
+
+        intersectionOf2Arrays(a,b);
+    }
+}
